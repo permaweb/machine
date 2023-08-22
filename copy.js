cf40a93a7d6d83b58f538179d0745a5b916ac038
Tom Wilson <tom@hyper.io>
2023-08-22 14:22:20 +0000

    update copy to take an argurment

--- copy.js
+++ copy.js
@@ -2,5 +2,8 @@ import fs from 'fs'
 
 const memes = fs.readdirSync('assets')
 
-memes.map((f, i) => fs.copyFileSync(`assets/${f}`, `permashade/${i + 1}.png`))
+const folder = process.argv[2]
+
+fs.mkdirSync(folder)
+memes.map((f, i) => fs.copyFileSync(`assets/${f}`, `${folder}/${i + 1}.png`))
 //memes.map((f, i) => console.log(f, i))
